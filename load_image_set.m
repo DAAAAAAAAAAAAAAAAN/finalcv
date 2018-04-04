c4f239fa87973d5b63b2f01e4a2184efacb6f240
Daan <stickfiguredaan@gmail.com>
2018-04-04 14:12:29 +0200

    Visualization 2.2 and 2.3

--- load_image_set.m
+++ load_image_set.m
@@ -1,7 +1,7 @@
-function images = load_image_set(category, type, max)
+function images = load_image_set(category, type, max, offset)
     images_dir = dir(fullfile('Caltech4', 'ImageData', strcat(category, '_', type), 'img*'));
     if length(images_dir) > max
-        images_dir = images_dir(1:max, :);
+        images_dir = images_dir(1+offset:max+offset, :);
     end
     
     n = length(images_dir);
--- main.m
+++ main.m
@@ -12,38 +12,56 @@ for i = 1:length(categories)
     category = categories{i};
     
     % load list of images from this set
-    images = load_image_set(category, 'train', image_count);
+    images = load_image_set(category, 'train', image_count, 0);
     
     % collect descriptors from all images densely
     for j = 1:length(images)
         image = images{j};
-        [f, d] = vl_phow(im2single(image), 'Step', 10); % default sift at the moment
+        [f, d] = vl_phow(im2single(image), 'Step', 20); % default sift at the moment
         descriptors{(i-1)*image_count + j} = d;
     end
 end
-disp("extracted features");
+all_descriptors = cat(2, descriptors{:});
+fprintf("extracted %i features\n", size(all_descriptors,2));
 
 %% 2.2 construct visual vocabulary
 % run kmeans to get visual words
-k = 400;
-all_descriptors = cat(2, descriptors{:});
-size(all_descriptors)
+k = 40;
+% [C, idx] = vl_ikmeans(all_descriptors, k);
 [idx, C] = kmeans(double(all_descriptors'), k);
 disp("constructed visual vocabulary");
+
+%% display some centroids
+figure(1);
+for i = 1:9
+    subplot(3,3,i);
+    %descriptor = C(:,i);
+    descriptor = C(i,:)';
+    vl_plotsiftdescriptor(descriptor);
+end
 
 %% 2.3 + 2.4 quantization + histogram
 for i = 1:length(categories)
     category = categories{i};
     
     % load list of images from this set
-    images = load_image_set(category, 'train', image_count);
+    images = load_image_set(category, 'train', image_count, 50);
     
     % represent each image as histogram
     for j = 1:length(images)
-        image = images{i};
-        [f, d] = vl_phow(im2single(image), 'Step', 10); % default sift at the moment
+        image = images{j};
+        [f, d] = vl_phow(im2single(image), 'Step', 20); % default sift at the moment
         % assign each descriptor to cluster
         idx = dsearchn(C, double(d'));
-        h = histcounts(idx, k, 'BinMethod','integers', 'Normalization', 'probability');
+        h = histcounts(idx, k, 'BinMethod', 'integers', 'Normalization', 'probability');
+        figure(2);
+        subplot(1,2,1);
+        histogram(idx, k);
+        subplot(1,2,2);
+        imshow(image);
+        
+        waitforbuttonpress
     end
 end
+
+
